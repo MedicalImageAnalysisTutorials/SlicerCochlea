85da77280ddcaa4d9461c33c4061bf420c925760
idhamari <ibr_ex@yahoo.com>
2018-10-24 17:40:41 +0200

    Update CochleaSeg.py
    remov tk support

--- CochleaSeg/CochleaSeg.py
+++ CochleaSeg/CochleaSeg.py
@@ -22,14 +22,6 @@
 #                                                                                     #  
 #======================================================================================
 
-try:
-    # for Python2
-    import Tkinter   ## capitalized T in Tkinter 
-except ImportError:
-    # for Python3
-    import tkinter   ## lowercase 't' in tkinter here
-
-import tkFileDialog
 import os, re , datetime, time ,shutil, unittest, logging, zipfile, urllib2, stat,  inspect
 import sitkUtils, sys ,math, platform  
 import  numpy as np, SimpleITK as sitk
