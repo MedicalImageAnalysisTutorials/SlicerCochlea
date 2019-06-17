2b59f2aae08cc25b659bf5b4b82d5c10c32bc20a
idhamari <idhamari@uni-koblenz.de>
2019-06-17 10:00:24 +0200

    fix link2me file

--- link2Me.sh
+++ link2Me.sh
@@ -10,6 +10,6 @@ mv $ewd1/$ext/$ewd2/CochleaReg.py $ewd1/$ext/$ewd2/CochleaReg.py.bk
 ln -s $PWD/CochleaReg/CochleaReg.py $ewd1/$ext/$ewd2/CochleaReg.py  
 mv $ewd1/$ext/$ewd2/CochleaSeg.py $ewd1/$ext/$ewd2/CochleaSeg.py.bk
 ln -s $PWD/CochleaSeg/CochleaSeg.py $ewd1/$ext/$ewd2/CochleaSeg.py  
-mv $ewd1/$ext/$ewd2/VisSimCommon/VisSimCommon.py $ewd1/$ext/$ewd2/VisSimCommon.py.bk
+mv $ewd1/$ext/$ewd2/VisSimCommon.py $ewd1/$ext/$ewd2/VisSimCommon.py.bk
 ln -s $PWD/VisSimCommon/VisSimCommon.py $ewd1/$ext/$ewd2/VisSimCommon.py
 
