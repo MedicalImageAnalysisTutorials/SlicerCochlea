ab0b41790c1b13c7815cf73ddd474e361a919a18
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2019-06-18 01:48:56 -0400

    STYLE: Remove unused test scripts

--- CochleaSeg/Testing/Python/CochleaSegmentationTest.py
+++
@@ -1,8 +0,0 @@
-# run:  ~/sw/Slicer-4.10.0/Slicer --no-main-window --python-script  CochleaSegmentationTest.py
-#TODO when called from test, no display. add number instead of boolean 0:internal, 1, external, 2 test
-
-
-import CochleaSeg
-
-CT = CochleaSeg.CochleaSegTest()
-CT.runExternaltest()
